38a3babf5cd96e302255b60237fdbe24b2cb1035
celebrater <tkgm45nbchk@gmail.com>
2022-01-21 18:26:54 +0900

    clean up code and deleted unnecessary files

--- ajax.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$ip = isset($_POST['ip']) ? $_POST['ip'] : null;
-
-//$result = //何らかの処理
-print json_encode($result);
--- index.php
+++ index.php
@@ -5,3 +5,5 @@ require_once "module.php";
 $ip = $_SERVER["REMOTE_ADDR"];
 
 add_log($ip);
+
+var_dump($ip);
